fbf5a65a0d2a7729920ce5f6980697cf4b8302bf
Dilsin Amirtharajah <DilsinAmirtharajahUSA@gmail.com>
2023-09-14 14:08:18 -0400

    Formatted with MS C formatter

--- variables.c
+++ variables.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main()
+{
 
     char characterName[] = "Messi";
     int characterAge = 36;
     printf("There once was a man named %s who was %d years old\n", characterName, characterAge);
     printf("%s is the best football player in the world.\n", characterName);
-    characterAge = 100; /*Testing age change mid line*/
+    characterAge = 100; /*Testing age change mid*/
     printf("%s really loved his job.\n", characterName);
     printf("But did not like being %d.\n", characterAge);
     return 0;
