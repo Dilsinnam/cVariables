f872b74abd8c23cd56f30e8386460b170a252e58
Dilsin Amirtharajah <DilsinAmirtharajahUSA@gmail.com>
2023-09-14 13:53:17 -0400

    Initial Commit

---
+++ variables.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+
+    char characterName[] = "Messi";
+    int characterAge = 36;
+    printf("There once was a man named %s who was %d years old\n", characterName, characterAge);
+    printf("%s is the best football player in the world.\n", characterName);
+    characterAge = 100; /*Testing age change mid line*/
+    printf("%s really loved his job.\n", characterName);
+    printf("But did not like being %d.\n", characterAge);
+    return 0;
+}
